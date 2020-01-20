ac29f053f1451e4f51bc28f74e8ba557e3e77189
ShonPozner <59409570+ShonPozner@users.noreply.github.com>
2020-01-20 18:19:11 +0200

    add server class

---
+++ Server.h
@@ -0,0 +1,32 @@
+//
+// Created by shon on 11.1.2020.
+//
+
+#ifndef EX4_SERVER_SIDE_SERVER_H_
+#define EX4_SERVER_SIDE_SERVER_H_
+
+#include "../clientHandle/ClientHandler.h"
+namespace server_side {
+    /**
+    * Server interface
+    */
+
+  class Server {
+   protected:
+    bool _stopFlag;
+
+  public:
+    virtual void open(int port,ClientHandler *clientHandler) = 0;
+    virtual void stop() = 0;
+
+
+    virtual ~Server() = default;
+
+    Server(){
+      _stopFlag = false;
+    }
+  };
+}
+
+
+#endif //EX4_SERVER_SIDE_SERVER_H_
